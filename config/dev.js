e21a1d69f09d19fbf79d12f2e2a9accdc02a2fa4
draganm82 <draganm1982@live.com>
2019-01-29 22:02:00 +0100

    add file

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+// Don't be like me and commit this file!
+// These keys have been disabled, but remain here for reference purposes.
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://draganm:dmm61252@ds261138.mlab.com:61138/blog_dev',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAIWSXFHRM7F6Z3NWQ',
+  secretAccessKey: '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'
+};
